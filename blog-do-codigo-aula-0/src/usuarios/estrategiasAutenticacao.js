3c678f9a4cd2b6c4f436d7dd5db11ebfd82fef7c
edemuner <eduardood15@hotmail.com>
2022-03-09 21:53:11 -0300

    added a checking function in estrategiasAutenticacao.js for verifying if the token exists in black list, it calls a function from manipulaBlackList, and it's called inside bearerStrategy

--- blog-do-codigo-aula-0/src/usuarios/estrategiasAutenticacao.js
+++ blog-do-codigo-aula-0/src/usuarios/estrategiasAutenticacao.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const { InvalidArgumentError } = require('../erros')
 const BearerStrategy = require('passport-http-bearer').Strategy
 const jwt = require('jsonwebtoken')
+const blacklist = require('../../redis/manipulaBlacklist')
 
 
 function verificaUsuario(usuario){
@@ -17,6 +18,13 @@ async function verificaSenha(senha, senhaHash){
     const senhaValida = await bcrypt.compare(senha, senhaHash)
     if (!senhaValida){
         throw new InvalidArgumentError('E-mail ou senha inválidos')
+    }
+}
+
+async function verificaTokenNaBlacklist(token){
+    const tokenNaBlackList = await blacklist.contemToken(token)
+    if (tokenNaBlackList){
+        throw new jwt.JsonWebTokenError('Token inválido por logout')
     }
 }
 
@@ -41,6 +49,7 @@ passport.use(
     new BearerStrategy(
         async (token, done) => {
             try{
+                await verificaTokenNaBlacklist(token)
                 const payload = jwt.verify(token, process.env.CHAVE_JWT)
                 const usuario = await Usuario.buscaPorId(payload.id)
                 done(null, usuario, { token: token })
